a6750eeea9c6af9f60493d06f6add9912aad98a9
iñaki <ienekamartinezg@gmail.com>
2020-07-08 09:55:32 +0200

    lletresRepetides

---
+++ js/lletresRepetides.js
@@ -0,0 +1,38 @@
+//FASE 1
+/* var nombre=["I","Ñ","A","K","I"];
+
+var contador=0;
+
+while (contador<nombre.length){
+    console.log(nombre[contador]);
+    contador++;
+}; */
+
+//FASE 2
+/* var nombre=["I","Ñ","A","K","I"];
+var vocal=["A","E","I","O","U"];
+for (let letra in nombre) {
+    if(vocal.includes(nombre[letra])){
+    console.log("He trobat la VOCAL: " + nombre[letra]);
+    }
+    else{
+    console.log("He trobat la CONSONANT: " + nombre[letra]);
+    }
+}; */
+//FASE 3
+/* const nombre=["I","Ñ","A","K","I"];
+
+const cantidadNombres = nombre.reduce((contadorNombre, nombre) =>{
+    contadorNombre[nombre] = (contadorNombre[nombre] || 0) + 1;
+    return contadorNombre;
+}, {});
+
+console.log(cantidadNombres); */
+
+//FASE 4
+/*const nombre=["I","Ñ","A","K","I"];
+const apellido=["M","A","R","T","I","N","E","Z"]
+
+nombre.push(" ");
+const FullName=[].concat(nombre,apellido);
+console.log(FullName);*/
