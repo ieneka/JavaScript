368b1f59c54d1361c76e2d41132d877a7de015cf
ieneka <60981008+ieneka@users.noreply.github.com>
2020-09-05 21:46:13 +0200

    Update objetos_color.js

--- js/objetos_color.js
+++ js/objetos_color.js
@@ -1,23 +1,17 @@
-var click=Boolean (false);
-function addRates(value){ 
-    for(var i=1; i<=value; i++) {
-        document.getElementById("autobus" +i).classList.add("active");
-    }
-}
-function reset(value){
-    if(click===false){
-        for(var i=1; i<=value; i++){
-            document.getElementById("autobus" +i).classList.remove("active");
-        }
-    }
-    else{
 
+function addRates(value){
+        for(var i=1; i<=value; i++) {
+        document.getElementById("autobus"+i).classList.add("active");
+        }
+}
+function removeRates(){
+    for(var i=1; i<=5; i++) {
+        document.getElementById("autobus"+i).classList.remove("active");
     }
-    
+
 } 
-function clickar(){
-    click=Boolean (true);
-}
-function clickReset(){
-    click=Boolean (false);
-}
+$('.bus').on('click', function(e) {
+    if (e.target !== this)
+      return;  
+    removeRates();
+});
